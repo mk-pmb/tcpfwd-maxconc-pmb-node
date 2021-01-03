1b0e477cd64c5e9bcd60947e9862378aedf8398d
mk-pmb <mk-pmb@users.noreply.github.com>
2021-01-03 03:14:58 +0100

    v0.2.13: Add build script for static version

---
+++ build/compile_static_bundle.js
@@ -0,0 +1,12 @@
+﻿// eslint-disable-next-line spaced-comment
+/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+require('p-fatal');
+require('esbrowserify-pmb')({
+  srcAbs: require.resolve('..'),
+  targetPlatform: 'nodejs',
+  saveAs: './node_modules/tcpfwd.static.js',
+  verbosity: 1,
+  minify: false,  // saves about 5% => not worth the obfuscation.
+});
